e6963667389b2b3a18ac6237323e95eb4fd71e0d
Youssef El idrissi <joseph@Youssefs-MacBook-Pro.local>
2023-03-31 01:22:32 +0000

    learning ...

---
+++ learning_concepts/iostream.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+# include <stdlib.h>
+
+int main(void)
+{
+    int a;
+    int b;
+
+    std::cout << "HELLO WORLD" << std::endl << "HEY JOSEPH" << "DEXTER" << std::endl;
+    std::cin >> a;
+    std::cout << "VAL OF A" << std::endl << 
+
+    return (0);
+
+}
