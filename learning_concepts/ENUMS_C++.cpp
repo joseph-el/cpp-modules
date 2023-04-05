99f27fafaf53f1108fe02ce320952160dd8399e0
Youssef El idrissi <joseph@Youssefs-MacBook-Pro.local>
2023-04-05 11:40:36 +0000

    Learning ...

--- learning_concepts/ENUMS_C++.cpp
+++ learning_concepts/ENUMS_C++.cpp
@@ -69,3 +69,4 @@ int main(void)
 #include <stdlib.h>
 #include <stdlib.h>
 #include <stdlib.h>
+#include <stdlib.h>
--- learning_concepts/Visibility_C++.cpp
+++ learning_concepts/Visibility_C++.cpp
@@ -50,3 +50,4 @@ int main(){TestRun ret}
 #include <stdlib.h>
 #include <stdlib.h>
 #include <stdlib.h>
+#include <stdlib.h>
