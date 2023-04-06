08f9e576d2c76ce5bb0b045d170180ba90691be4
Youssef El idrissi <youssefelidrissi200@gmail.com>
2023-04-06 01:31:23 +0000

    learning ...

--- learning_concepts/ENUMS_C++.cpp
+++ learning_concepts/ENUMS_C++.cpp
@@ -1,72 +1,52 @@
-# include <iostream>
-# include <stdio.h>
+#include <iostream>
 
-# if !defined(LOG_)
-    #define LOG_(X) std::cout << X << std::endl
-#endif
+using namespace std;
 
-void    set_(int &data) {
-    data = 12;
-}
+typedef short t_class;
 
-struct refrence {
-    int i;
-    short s;
-    char *nn;
+class s_Rectangle 
+{
+    int width, height;
+    
+public:
+
+    void set_values (int,int);
+    int area() {return width * height;}
 };
 
-typedef struct refrence t_ref;
-# define FILL (1 << 4)
+class Rectangle {
+  int width, height;
+public:
+  Rectangle(int x, int y) : width(x), height(y) {}
+  int area(void) { return width * height; }
+};
 
-void    fill(t_ref &leet, int32_t flag){
-    if (flag & ~FILL){
-        using namespace std;
-        std::cout << "leet.i = " << leet.i << endl;
-        std::cout << "leet.s = " << leet.s << endl;
-        std::cout << "leet.nn = " << leet.nn << endl;
-        return ;
-    }
-    leet.i = 42;
-    leet.nn = strdup("Hello World Iam joseph");
-    leet.s = -42;
+void s_Rectangle::set_values (int x, int y){
+  width = x;
+  height = y;
 }
 
-int main(){
-
+int main () 
 {
-    // try for structs
-    t_ref ref ; memset(&ref, 0, sizeof(ref));
-    ref.nn = nullptr;
-    ref.i = -2;
-    ref.s = -4;
-    fill(ref, FILL);
-    fill(ref, 0);
+  s_Rectangle rect;
 
-    return (ENETDOWN);
-}
-
-    {
-        // try for Var
-        int x;
-        x = -33;
-        LOG_(x);
-        set_(x);
-        using namespace std;
-        std::cout << "after" << endl;
-        LOG_(x);
-
-        return EXIT_SUCCESS;
-    }
-
-    int x;
-    int *ptr;
-
-    x = 10;
-    ptr = &x;
-
-    printf("CHECK ADDRESS |%p|\n", &x);
-    int& ret = x;
-    printf("CHECK ADDRESS |%p|\n", &ret);
+    rect.set_values (3,4);
     
-    exit(EXIT_SUCCESS);
+    cout << "test: " << rect.area();
+  return 0;
+    
+  Rectangle obj (3, 4);
+  Rectangle * foo, * bar, * baz;
+  foo = &obj;
+  bar = new Rectangle (5, 6);
+  baz = new Rectangle[2] { {2,5}, {3,6} };
+  cout << "objs area: " << obj.area() ;
+  cout << "*foos area: " << foo->area();
+  cout << "*bars area: " << bar->area() <<;
+  cout << "baz[0]s area:" << baz[0].area();
+  cout << "baz[1]s area:" << baz[1].area();       
+  delete bar;
+  delete[] baz;
+
+  return 0;
 }
