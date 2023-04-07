ce13a0e5054bf130afabc4f40cd9e41431a7b341
Youssef El idrissi <youssefelidrissi200@gmail.com>
2023-04-07 21:53:50 +0000

    learning ...

---
+++ learning_concepts/Inheritance.cpp
@@ -0,0 +1,55 @@
+# include <iostream>
+# include <stdio.h>
+# include "Colors.hpp"
+struct VEC
+{
+    int _FI_, _LA_;
+    int64_t _LC_:10;
+};
+
+class UF{
+    protected :
+        struct VEC *__mp;
+        bool       *mode;
+    public :
+        UF();
+    void (*___leet___)(void);
+}
+
+
+class newClass : public UF{
+    bool    t_ret;
+    public :
+        void print_Class();
+};
+
+void    newClass::print_Class(void){
+    this->t_ret;
+
+}
+
+
+
+void leet(void){
+    using namespace std;
+    cout << RED;
+    cout << "UP--UP--UP" << endl;
+    cout << WHITE;
+}
+
+UF::UF(){
+    __mp = new struct VEC;
+    __mp->_FI_ = 42;
+    __mp->_LA_ = -42;
+    __mp->_LC_ = 1;
+    mode = new bool;
+    *mode = false;
+    ___leet___ = &leet;
+    std::cout << "INIT EXIT_SUCCESS" << std::endl;
+}
+
+
+
+
+int main(){
+}
