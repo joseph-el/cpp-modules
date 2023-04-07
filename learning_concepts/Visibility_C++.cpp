42b4ea52cb85f409b3b3543a99c080ee8298d3f9
Youssef El idrissi <youssefelidrissi200@gmail.com>
2023-04-07 22:07:32 +0000

    learning ...

--- learning_concepts/Visibility_C++.cpp
+++ learning_concepts/Visibility_C++.cpp
@@ -1,68 +1,51 @@
-# pragma once
 # include <iostream>
-# include <stdlib.h>
-# include <errno.h>
-# include <limits>
+# include <stdbool.h>
+
+
+typedef class s_class
+{
+    private :
+        short ret;
+        short get;
+        std::string name;
+    public :
+        void    init();
+        void    disp();
+
+    friend void friend_(s_class & class_);
+}               t_class;
+
+void    t_class::init(void)
+{
+    this->get = ENAMETOOLONG;
+    this->ret = ENOLINK;
+    this->name = (char *)strdup("HEY MY NAME JOSEPH");
+}
 
 using namespace std;
 
-int32_t main(int32_t argc, char **argv)
+void    t_class::disp(void)
 {
-
-    {
-        char **str;
-        std::int32_t size = ENAMETOOLONG;
-        
-        str = new char*[size];
-        for (size_t i = 0; i < ENAMETOOLONG; i++)
-        {
-            str[i] = new char[ENOTBLK];
-            strncpy((char *)str[i], "LOLOLOLOLOLOLO", sizeof("LOLOLOLOLOLOLO") + 1);
-            std::cout << str[i] << (char)32 << i + 1 << endl;
-        }
-        return (ENOPROTOOPT);
-    }
-    
-    {
-        int *arr;
-        // arr = new int(INT32_MAX + INT16_MAX); // overflow?✅
-        std::cout << *arr << std::endl;
-        return (ENOMEM);
-    }
-
-    {
-        int *arr;
-        arr = new int[10], memset(arr, -1, sizeof(int) * 10);
-        for (size_t i = 0; i < 10; i++)
-        {
-            std::cout << arr[i] << endl;
-        }
-        delete arr;
-        *(arr + 4) = 43; // use after free ✅
-        // system("c++ -fsanitize=address -g memory.cpp");
-        return EXIT_SUCCESS; 
-        int *ptr;
-        ptr = new int;
-        *ptr = 9;
-        delete ptr;
-        ptr = new int;
-        *ptr = (char)22;
-        printf("check |%d|\n", *ptr);
-        system("leaks a.out");
-    }
-
-
-    return 1;
-    int *ptr = (int *)malloc(sizeof(int) * 100);
-    *(ptr + 23) = 42;
-    std::cout << *(ptr + 23);
-    free(ptr);
-    std::cout << "-----------------" << endl;
-    std::cout << *(ptr + 23) << endl;
-    std::cout << "-----------------" << endl;
-    *(ptr + 200000) = 1337;
-    std::cout << *(ptr + 20000) << endl;
-    std::cout << "-----------------" << endl;
-    
-    return 1;
+    std::cout << "Edited " << this->ret << endl;
+    std::cout << "JOSEPH " << this->get << endl;
+    std::cout << "ORY " << this->name << endl;
 }
+
+void    friend_(t_class &js)
+{
+    js.get = 1337;
+    js.ret = 42;
+}
+
+int main(void)
+{
+    t_class ret;
+
+    ret.init();
+    ret.disp();
+    friend_(ret);
+    ret.disp();
+
+    return (EXIT_SUCCESS);
+}
+
