f86bd047886297fdcb002cf42977f4bcaf6d5245
Youssef El idrissi <youssefelidrissi200@gmail.com>
2023-04-15 18:17:39 +0000

    Init() Module

--- module/NodePe.js
+++ module/NodePe.js
@@ -20,3 +20,42 @@ for (let name of readdirSync(".")) {
     }
   }
 }
+
+const PJSON = require("./pseudo_json")
+let fs = require("fs")
+let acorn = require("acorn")
+let varify = require("./varify")
+
+let file = process.argv[2]
+let chapNum = Number(file.match(/^\d*/)[0])
+let doRun = ![10, 11, 20, 21].includes(chapNum)
+let input = fs.readFileSync(file, "utf8")
+
+
+let meta = /\{\{meta (.*?)\}\}\n/.exec(input)
+let include = /\bload_files: (\[[^\]]+\])/.exec(meta ? meta[1] : "")
+if (include) JSON.parse(include[1]).forEach(function(fileName) {
+  if (fileName == "code/chapter/" + file.replace(/md/, "js")) return
+  let text = fs.readFileSync("html/" + fileName)
+  if (!/\/\/ test: no/.test(text))
+    baseCode += text
+})
+
+function wrapTestOutput(snippet, config) {
+  let output = "", m, re = /\/\/ → (.*\n)((?:\/\/   .*\n)*)/g
+  while (m = re.exec(snippet)) {
+    output += m[1]
+    if (m[2]) output += m[2].replace(/\/\/   /g, "")
+  }
+  return "console.clear();\n" + snippet + "console.verify(" + JSON.stringify(output) + ", " + JSON.stringify(config) + ");\n"
+}
+
+function wrapForError(snippet, message) {
+  return "try { (function() {\n" + snippet + "})();\n" +
+    "cX" +
+    JSON.stringify(message) + ") }\n"
+}
+
+function pos(index) {
+  return "line " + (input.slice(0, index).split("\n").length + 1)
+}
